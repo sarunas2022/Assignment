4b57f2c2e9fb8e032577b712176e6143548ef21b
Šarūnas Grabauskas <sarunas.grabauskas@gmail.com>
2023-01-26 17:21:54 +0200

    implemented filtering by region together with sort

--- src/App.js
+++ src/App.js
@@ -4,62 +4,96 @@ import { Posts } from './components/Posts';
 import { Pagination } from './components/Pagination';
 
 function App() {
+    // for data fetching and status
     const [data, setData] = useState([]);
     const [status, setStatus] = useState('idle');
     // for pagination
     const [currentPage, setCurrentPage] = useState(1);
     const [postsPerPage] = useState(20);
-
     // for sorting
     const [sort, setSort] = useState('ascending');
-
-    // getting data from API
-    const fetchData = async () => {
-        try {
-            setStatus('loading');
-            const data = await fetch(
-                'https://restcountries.com/v2/all?fields=name,region,area'
-            );
-            const response = await data.json();
-            setData(response);
-
-            setStatus('fulfilled');
-        } catch (err) {
-            console.log(err);
-            setStatus('rejected');
-        }
-    };
-
-    // function to sort post ascending or descending alphabetically
-    const sortingPosts = () => {
-        if (sort === 'Descending') {
-            setSort('ascending');
-            data.sort((a, b) => {
-                if (a.name < b.name) return -1;
-                return 1;
-            });
-        } else {
-            console.log('clicked');
-            setSort('Descending');
-            data.sort((a, b) => {
-                if (a.name > b.name) return -1;
-                return 1;
-            });
-        }
-    };
-    // Sorting ascending
-
-    // Sorting Descending
+    // for filtering by region- store the selected region option value
+    const [selectedRegion, setSelectedRegion] = useState('All regions');
+    const [filteredData, setFilteredData] = useState([]);
 
     // Fetching data on page load
     useEffect(() => {
+        // getting data from API
+        const fetchData = async () => {
+            try {
+                setStatus('loading');
+                const data = await fetch(
+                    'https://restcountries.com/v2/all?fields=name,region,area'
+                );
+                const response = await data.json();
+                setData(response);
+                console.log(response);
+
+                setStatus('fulfilled');
+            } catch (err) {
+                console.log(err);
+                setStatus('rejected');
+            }
+        };
         fetchData();
     }, []);
+
+    // function to sort post ascending or descending alphabetically
+    const sortingPosts = () => {
+        // checking if data was filtered if not sorting data array
+        if (selectedRegion === 'All regions') {
+            if (sort === 'Descending') {
+                setSort('ascending');
+                data.sort((a, b) => {
+                    if (a.name < b.name) return -1;
+                    return 1;
+                });
+            } else {
+                setSort('Descending');
+                data.sort((a, b) => {
+                    if (a.name > b.name) return -1;
+                    return 1;
+                });
+            }
+        } else {
+            if (sort === 'Descending') {
+                setSort('ascending');
+                filteredData.sort((a, b) => {
+                    if (a.name < b.name) return -1;
+                    return 1;
+                });
+            } else {
+                setSort('Descending');
+                filteredData.sort((a, b) => {
+                    if (a.name > b.name) return -1;
+                    return 1;
+                });
+            }
+        }
+        // after sorting sets back current page to the first one
+        setCurrentPage(1);
+    };
+
+    // filtering posts by region
+    const filterByRegion = (event) => {
+        setSelectedRegion(event.target.value);
+    };
+
+    useEffect(() => {
+        const filteredByRegion = [...data].filter(
+            (country) => country.region === selectedRegion
+        );
+        setFilteredData(filteredByRegion);
+        setCurrentPage(1);
+    }, [data, selectedRegion]);
 
     // get current posts
     const indexOfLastPost = currentPage * postsPerPage;
     const indexOfFirstPost = indexOfLastPost - postsPerPage;
-    const currentPost = data.slice(indexOfFirstPost, indexOfLastPost);
+    const currentPost =
+        selectedRegion !== 'All regions'
+            ? filteredData.slice(indexOfFirstPost, indexOfLastPost)
+            : data.slice(indexOfFirstPost, indexOfLastPost);
 
     // change page - function passed down as props to pagination.jsx, returns
     // value of click event as pageNumber and its used sets CurrentPage
@@ -71,7 +105,21 @@ function App() {
             <div className='ButtonsContainer'>
                 <div className='filterButtons'>
                     <button>Smaller Then LT</button>
-                    <button>Oceanic region</button>
+                    <div>
+                        <label for='region'>Choose a region:</label>
+                        <select onChange={filterByRegion} id='region'>
+                            <option value='All regions'>All regions</option>
+                            <option value='Africa'>Africa</option>
+                            <option value='Americas'>Americas</option>
+                            <option value='Antarctic Ocean'>
+                                Antarctic Ocean
+                            </option>
+                            <option value='Asia'>Asia</option>
+                            <option value='Europe'>Europe</option>
+                            <option value='Oceania'>Oceania</option>
+                            <option value='Polar'>Polar</option>
+                        </select>
+                    </div>
                 </div>
                 <div className='sortingButtons'>
                     <button onClick={() => sortingPosts()}>
@@ -82,7 +130,11 @@ function App() {
             <Posts data={currentPost} status={status} />
             <Pagination
                 postsPerPage={postsPerPage}
-                totalPosts={data.length}
+                totalPosts={
+                    selectedRegion !== 'All regions'
+                        ? filteredData.length
+                        : data.length
+                }
                 paginate={paginate}
                 currentPage={currentPage}
             />
