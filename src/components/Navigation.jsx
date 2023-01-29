4e032f5c7e9058b6d1a5364718fe6dfb4d018a1d
Šarūnas Grabauskas <sarunas.grabauskas@gmail.com>
2023-01-29 11:31:33 +0200

    Added world icon to navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './Navigation.module.scss';
+import PublicIcon from '@mui/icons-material/Public';
 
 export const Navigation = () => {
     return (
         <div className={styles.navigationBar}>
+            <PublicIcon fontSize='large' />
             <h1>World Countries</h1>
         </div>
     );
