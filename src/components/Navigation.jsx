8dc9669d823b5cc0bfa30165bfee5b53c23bdae7
Šarūnas Grabauskas <sarunas.grabauskas@gmail.com>
2023-01-29 10:53:17 +0200

    added navigation bar

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Navigation.module.scss';
+
+export const Navigation = () => {
+    return (
+        <div className={styles.navigationBar}>
+            <h1>World Countries</h1>
+        </div>
+    );
+};
